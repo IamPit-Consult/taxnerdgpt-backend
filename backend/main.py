2b5056028f1beae4e1b8e12f02a759375ca669a9
Jordan Peete <jpeete@pit-consult.com>
2025-12-12 00:01:48 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "https://ai.taxnerd.us", "https://app.taxnerd.us", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
